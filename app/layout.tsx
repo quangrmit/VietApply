cc89efb6ac481f56a201ac07bad4fdbc0a695c7f
Quang Nguyen <s3927198@rmit.edu.vn>
2025-02-11 21:49:42 +0700

    add tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,7 +30,7 @@ export default function RootLayout({
       >
         {children}
       </body>
-      {/* <GoogleTagManager gtmId='G-2D4QZYP55V'/> */}
+      <GoogleTagManager gtmId='G-2D4QZYP55V'/>
     </html>
   );
 }
