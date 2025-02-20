674cdcb6dc432bd438761d94a11d1aa21e0484cc
Quang Nguyen <s3927198@rmit.edu.vn>
2025-02-20 11:50:16 +0700

    fixed minor bug

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -13,6 +13,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     
     await redis.lpush("job_queue", JSON.stringify(message));
 
-    res.status(200).send("Success");
+    res.status(200).send({"status": "success"});
 
 }
